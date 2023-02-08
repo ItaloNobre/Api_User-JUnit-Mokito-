54aab84d2266877bf434745e52c82618d8770380
Italo Nobre <italonobre@ads.fiponline.edu.br>
2023-02-08 16:56:56 -0300

    retornando DTO para UserContoller

--- src/main/java/br/com/api/testes/controller/UserContoller.java
+++ src/main/java/br/com/api/testes/controller/UserContoller.java
@@ -1,7 +1,9 @@
 package br.com.api.testes.controller;
 
 import br.com.api.testes.domain.User;
+import br.com.api.testes.domain.dto.UserDTO;
 import br.com.api.testes.services.impl.UserServiceImpl;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserContoller {
 
     @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
     UserServiceImpl service;
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body( service.findById(id));
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
     }
 }
--- src/main/java/br/com/api/testes/domain/dto/UserDTO.java
+++ src/main/java/br/com/api/testes/domain/dto/UserDTO.java
@@ -11,7 +11,7 @@ public class UserDTO {
 
     private Integer id;
     private String name;
-    private String email;
+    //private String email;
     private String password;
 
 }
