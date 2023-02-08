847fb4919267fe6ad61d6d588bf696162584a5b3
Italo Nobre <italonobre@ads.fiponline.edu.br>
2023-02-08 11:14:07 -0300

    add method findById

--- src/main/java/br/com/api/testes/controller/UserContoller.java
+++ src/main/java/br/com/api/testes/controller/UserContoller.java
@@ -1,6 +1,8 @@
 package br.com.api.testes.controller;
 
 import br.com.api.testes.domain.User;
+import br.com.api.testes.services.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserContoller {
 
+    @Autowired
+    UserServiceImpl service;
+
     @GetMapping("/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(new User(1,"italo","email","124d"));
+        return ResponseEntity.ok().body( service.findById(id));
     }
 }
