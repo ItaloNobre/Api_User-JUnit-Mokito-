ad05bd46c61f97914340786f80e4535e1a5b7f6b
Italo Nobre <italonobre@ads.fiponline.edu.br>
2023-02-08 10:52:08 -0300

    primeiro commit

---
+++ src/main/java/br/com/api/testes/domain/User.java
@@ -0,0 +1,24 @@
+package br.com.api.testes.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity(name = "usuario")
+public class User {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+}
