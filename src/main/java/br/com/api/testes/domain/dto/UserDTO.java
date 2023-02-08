9452efd4faccb32d2b0b4807cc9f8ea6b5316afa
Italo Nobre <italonobre@ads.fiponline.edu.br>
2023-02-08 16:25:46 -0300

    Class UserDTO

---
+++ src/main/java/br/com/api/testes/domain/dto/UserDTO.java
@@ -0,0 +1,17 @@
+package br.com.api.testes.domain.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+
+}
