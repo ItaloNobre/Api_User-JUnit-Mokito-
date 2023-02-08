0bd04bc5d02c8856da56c50a0f593d82b737f835
Italo Nobre <italonobre@ads.fiponline.edu.br>
2023-02-08 11:13:43 -0300

    Class UserServiceImpl

---
+++ src/main/java/br/com/api/testes/services/impl/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package br.com.api.testes.services.impl;
+
+import br.com.api.testes.domain.User;
+import br.com.api.testes.repositories.UserRepository;
+import br.com.api.testes.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository repository;
+
+    @Override
+    public User findById(Integer id) {
+        Optional<User> obj = repository.findById(id);
+       return obj.orElse(null);
+    }
+}
