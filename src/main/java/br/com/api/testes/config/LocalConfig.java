a0a2c62886cb7c6655a32678e6daae667aff24fe
Italo Nobre <italonobre@ads.fiponline.edu.br>
2023-02-08 14:11:10 -0300

    profile local

---
+++ src/main/java/br/com/api/testes/config/LocalConfig.java
@@ -0,0 +1,25 @@
+package br.com.api.testes.config;
+
+import br.com.api.testes.domain.User;
+import br.com.api.testes.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
+
+@Configuration
+@Profile("local")
+public class LocalConfig {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Bean
+    public void startDB(){
+        User u1 = new User(null, "joao","aa@gmail.com", "123");
+        User u2 = new User(null, "binho","binho@gmail.com", "123");
+
+        userRepository.saveAll(List.of(u1,u2));
+    }
+}
