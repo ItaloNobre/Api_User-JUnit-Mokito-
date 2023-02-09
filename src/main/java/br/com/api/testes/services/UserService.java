006a4f3a206f9d178d883a13956f0bcf0c7facc9
Italo Nobre <italonobre@ads.fiponline.edu.br>
2023-02-09 18:47:12 -0300

    criando metodos para User, post, put, delete

--- src/main/java/br/com/api/testes/services/UserService.java
+++ src/main/java/br/com/api/testes/services/UserService.java
@@ -1,8 +1,15 @@
 package br.com.api.testes.services;
 
 import br.com.api.testes.domain.User;
+import br.com.api.testes.domain.dto.UserDTO;
+
+import java.util.List;
 
 public interface UserService {
 
     User findById(Integer id);
+    List<User> findAll();
+    User create (UserDTO obj);
+    User update(UserDTO obj);
+    void delete(Integer id);
 }
--- src/main/java/br/com/api/testes/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/api/testes/services/impl/UserServiceImpl.java
@@ -1,12 +1,16 @@
 package br.com.api.testes.services.impl;
 
 import br.com.api.testes.domain.User;
+import br.com.api.testes.domain.dto.UserDTO;
 import br.com.api.testes.repositories.UserRepository;
 import br.com.api.testes.services.UserService;
+import br.com.api.testes.services.exceptions.DataIntegrityViolationException;
 import br.com.api.testes.services.exceptions.ObjectNotFoundException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -15,9 +19,42 @@ public class UserServiceImpl implements UserService {
     @Autowired
     UserRepository repository;
 
+    @Autowired
+    ModelMapper modelMapper;
+
     @Override
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+    }
+
+    @Override
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public User create(UserDTO obj) {
+        findByEmail(obj);
+        return repository.save(modelMapper.map(obj, User.class));
+    }
+
+    @Override
+    public User update(UserDTO obj) {
+        findByEmail(obj);
+        return repository.save(modelMapper.map(obj,User.class));
+
+    }
+
+    @Override
+    public void delete(Integer id) {
+        repository.delete(findById(id));
+    }
+
+    private void findByEmail(UserDTO obj){
+        Optional<User> user = repository.findByEmail(obj.getEmail());
+        if(user.isPresent() && !user.get().getId().equals(obj.getId())){
+            throw new DataIntegrityViolationException("E-mail já cadastrado no sistema");
+        }
     }
 }
