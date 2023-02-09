ef9ccbc233b406771553aab7c215ec9eb135c6d0
Italo Nobre <italonobre@ads.fiponline.edu.br>
2023-02-09 18:39:36 -0300

    adicionando access write only em email e password

--- src/main/java/br/com/api/testes/domain/dto/UserDTO.java
+++ src/main/java/br/com/api/testes/domain/dto/UserDTO.java
@@ -1,5 +1,7 @@
 package br.com.api.testes.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 @Getter
@@ -11,7 +13,10 @@ public class UserDTO {
 
     private Integer id;
     private String name;
-    //private String email;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String email;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
 }
