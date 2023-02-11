a9f167831075e8864b33660621f0195e71c710cb
Italo Nobre <italonobre@ads.fiponline.edu.br>
2023-02-10 21:28:17 -0300

    teste para classe user service metodo update com sucesso

--- src/test/java/br/com/api/testes/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/api/testes/services/impl/UserServiceImplTest.java
@@ -115,7 +115,18 @@ class UserServiceImplTest {
     }
 
     @Test
-    void update() {
+    void whenUpdateThenReturnSuccess() {
+        Mockito.when(repository.save(Mockito.any())).thenReturn(user);
+
+        User response = service.update(userDTO);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
+
     }
 
     @Test
